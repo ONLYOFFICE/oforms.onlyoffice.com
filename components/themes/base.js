0c67d6c19f5724843c5d7f1722e06606bea41e64
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-11-02 13:51:49 +0300

    client: update package.json
    update package.json
    update themes/base
    update utils/global-colors

--- components/themes/base.js
+++ components/themes/base.js
@@ -2,7 +2,11 @@ import globalColors from "../utils/global-colors";
 const {
   white,
   gray,
-  orangeMain
+  grayMain,
+  graySecnodary,
+  grayHover,
+  orangeMain,
+  orangeHover
 } = globalColors;
 
 const Base = {
@@ -42,7 +46,7 @@ const Base = {
     lineHeight: ["74px", "53px", "38px", "32px", "24px", "19px", "19px"],
     fontWeight: 700,
   },
-  
+
   externalLink: {
     color: orangeMain,
     textDecoration: "underline",
@@ -52,6 +56,101 @@ const Base = {
     hoverTextDecoration: "none",
     hoverCursor: "pointer",
   },
+
+  textInput: {
+    textColor: gray,
+    textColorHover: grayHover,
+    textColorSecondaryType: graySecnodary,
+    textColorSecondaryTypeHover: gray,
+
+    backgroundColor: "#fff",
+    backgroundColorPrimaryHover: white,
+    backgroundColorDisabled: "#F9F9F9",
+    backgroundColorSuccess: "#F9FEEF",
+    backgroundColorError: "#FFF7F7",
+
+    border: "1px solid",
+    borderRadius: "3px",
+    borderColor: "#AAAAAA",
+    borderColorHover: "#666666",
+    borderColorDisabled: "#AAAAAA",
+    borderColorSuccess: "#8BB825",
+    borderColorError: "#CB0000",
+
+    position: "absolute",
+    display: "block",
+    boxSizing: "border-box",
+    boxShadow: "none",
+    width: "100%",
+    height: "56px",
+    padding: "0 16px",
+    margin: "0",
+    overflow: "auto",
+    fontSize: "16px",
+    fontWeight: "400",
+    lineHeight: "22px",
+    textAlign: "left",
+    verticalAlign: "center",
+    textDecoration: "none",
+    textTransform: "none",
+    textShadow: "none",
+    outline: "none",
+
+    placeholderColor: "#CCC",
+
+    label: {
+      color: "#AAAAAA",
+      colorHover: gray,
+      padding: "0 15px",
+      left: "2px",
+      top: "8px",
+      fontSize: "12px",
+    },
+
+    media: {
+      width: "100%",
+      fontSize: "13px",
+    },
+  },
+
+  button: {
+    textColor: white,
+    textColorTransparentType: grayMain,
+    textColorTransparentTypeHover: orangeMain,
+    textColorSecondaryType: white,
+    borderColorHover: orangeMain,
+    backgroundColorPrimary: orangeMain,
+    backgroundColorSecondary: grayMain,
+    backgroundColorPrimaryHover: orangeHover,
+    backgroundColorSecondaryHover: grayHover,
+
+    borderTransparentType: `1px solid ${grayMain}`,
+    borderRadius: "3px",
+
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    outline: "none",
+    boxSizing: "border-box",
+    fontWeight: "600",
+    margin: "0",
+    display: "inline-block",
+    textAlign: "center",
+    textDecoration: "none",
+    topVerticalAlign: "text-top",
+    middleVerticalAlign: "middle",
+    bottomVerticalAlign: "text-bottom",
+    lineHeight: "13px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    transitionDuration: "0.3s",
+
+    mobile: {
+      height: "48px",
+      width: "100%"
+    }
+  },
+
 };
 
 export default Base;
--- components/utils/global-colors.js
+++ components/utils/global-colors.js
@@ -1,7 +1,14 @@
 const globalColors = {
   white: "#ffffff",
+  
   gray: "#333",
+  grayMain: "#444444",
+  grayHover: "#555555",
+  grayTextInput: "#666666",
+  grayTextInputHover: "#F9F9F9",
+
   orangeMain: "#ff6f3d",
+  orangeHover: "#ff865c",
 };
 
 export default globalColors;
