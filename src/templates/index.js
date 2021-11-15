57e8d9cf908510fae6409ab534abd18953f34ef8
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-11-15 15:47:14 +0300

    src: added template
    added data file and templates

---
+++ src/templates/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+
+import Layout from "../../components/layout";
+import HeadSEO from "../sub-components/head-content";
+import Footer from "../sub-components/footer-content";
+
+const Template = ({ pageContext, ...rest }) => {
+
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation();
+
+  const { data, pathName } = pageContext;
+  const { seo } = data;
+  const { title, description } = seo;
+
+  return (
+    <Layout {...rest}>
+      <Layout.PageHead>
+        <HeadSEO
+          title={title}
+          metaDescription={description}
+          metaKeywords={title}
+        />
+      </Layout.PageHead>
+      <Layout.PageHeader>
+      </Layout.PageHeader>
+      <Layout.SectionMain>
+      </Layout.SectionMain>
+      <Layout.PageFooter>
+        <Footer language={language} t={t} />
+      </Layout.PageFooter>
+    </Layout>
+
+  );
+};
+
+export default Template;
+
+export const query = graphql`
+  query($language: String!) {
+    locales: allLocale(filter: { language: { in: [$language, "en"] } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
