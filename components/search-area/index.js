726ea644dabba6b80b020cb168aa91bd7cd6bc9b
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-11-17 17:14:31 +0300

    components: refactoring cmp search

--- components/search-area/index.js
+++ components/search-area/index.js
@@ -1,29 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledSearchArea from "./styled-search-area";
+
 import Text from "../text";
 import Box from "../box";
 import TextInput from "../text-input/";
-import IconButton from "../icon-button";
+
+import StyledSearchArea from "./styled-search-area";
+
 import SVG_Search from "../../static/icons/search-icon.react.svg"
 
-
-const SearchArea = () =>{
+const SearchArea = () => {
 
     const [value, isValue] = React.useState("");
 
-
-    
-
-    console.log("11");
-
-    return(
+    return (
         <StyledSearchArea>
             <Box
-                className="search_container "
+                className="search_container"
                 alignItems="center"
             >
-                <Text 
+                <Text
                     className="presearch_title"
                     overflow="unset"
                     textOverflow="clip"
@@ -31,24 +27,18 @@ const SearchArea = () =>{
                     color="#F5F5F5"
                 />
                 <TextInput
-                    value={value} 
+                    value={value}
                     onChange={(event) => isValue(event.target.value)}
                     type="text"
-                    className="search_input"                    
+                    className="search_input"
                     backgroundColor="#333"
                     color="#808080"
                     placeholder="Search name form, type, industry"
                     fontSize="16px"
                     colorHover="808080"
-
-                >
-                </TextInput>
-                <SVG_Search 
-                    
                 />
-                
+                <SVG_Search />
             </Box>
-            
         </StyledSearchArea>
 
     );
--- components/search-area/styled-search-area.js
+++ components/search-area/styled-search-area.js
@@ -1,33 +1,27 @@
 import styled from "styled-components";
 
-
 const StyledSearchArea = styled.div`
 background-color: #333;
 position: relative;
 width: 100%;
-    .search_container{
+
+    .search_container {
         margin: 0 auto;
         max-width: 958px;
         border-bottom: 1px solid #666;
     }
-    .presearch_title{
+
+    .presearch_title {
         line-height: 1em;
         padding-right: 24px;
         border-right: 1px solid #aaa;
         overflow: unset;
     }
-    .search_input{
-        
+    
+    .search_input {
         border: none;
         padding: 21px 118px 21px 15px;
         white-space: normal;
-        
-
     }
-   
-
-
-
-
 `;
 export default StyledSearchArea;
