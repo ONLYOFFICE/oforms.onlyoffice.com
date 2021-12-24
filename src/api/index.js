ef44e0176d6a5625255862cad063147aa2aa4fa9
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-12-24 11:37:07 +0300

    api: fix url api

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import { request } from "./api";
 export function getConfig(id) {
   return request({
     method: "get",
-    url: `/page/${id}`,
+    url: `/${id}`,
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
