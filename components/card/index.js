31070a34bec18a4f67bbaea99ff2556a2baebb13
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-01-07 09:28:31 +0300

    components: fix style cards

--- components/card/index.js
+++ components/card/index.js
@@ -32,8 +32,8 @@ const Card = ({ t, callback, arrayItems, ...rest }) => {
         flexDirection="column"
         alignItems="stretch"
       >
-        <ELink className="title-template" href={pathName} label={name} />
-        <Text className="subtitle-template" label={file_description} />
+        <ELink className="title-template text-overflow-templapte" href={pathName} label={name} />
+        <Text className="subtitle-template text-overflow-templapte" label={file_description[0]} />
         <ELink target="_blank" href={`/editor?fillform=${linkFillForm}`}>
           <Button
             isScale
--- components/card/styled-card.js
+++ components/card/styled-card.js
@@ -1,77 +1,81 @@
 import styled from "styled-components";
 
 const StyledCard = styled.div`
-display: flex;
-flex-direction: column;
-justify-items: stretch;
-max-width: 256px;
-max-height: 576px;
-background-color: #F9F9F9;
+  display: flex;
+  flex-direction: column;
+  justify-items: stretch;
+  max-width: 256px;
+  max-height: 576px;
+  background-color: #f9f9f9;
 
-.image-template {
-    max-width:254px;
-    border: 1px solid #E2E2E2;
+  .image-template {
+    max-width: 254px;
+    border: 1px solid #e2e2e2;
     border-bottom: none;
-}
+  }
 
-.card-template {
-    background-color: #F9F9F9;
-    border: 1px solid #E2E2E2;
-    padding: 24px 24px 10px 24px;
+  .card-template {
+    background-color: #f9f9f9;
+    border: 1px solid #e2e2e2;
+    padding: 22px 24px 0px 24px;
     max-height: 239px;
-}
+  }
 
-.title-template {
-    padding-bottom: 16px;
-    text-overflow: ellipsis;
-    white-space: break-spaces;
+  .title-template {
+    margin-bottom: 16px;
     height: 42px;
-    overflow: hidden;
-}
+  }
 
-.subtitle-template {
-    padding-bottom: 24px;
-    max-height: 42px;
-    height: auto;
+  .subtitle-template {
+    margin-bottom: 24px;
+    height: 42px;
+  }
+
+  .text-overflow-templapte {
+    -ms-text-overflow: ellipsis;
+    -o-text-overflow: ellipsis;
     text-overflow: ellipsis;
-    white-space: nowrap;
-}
+    overflow: hidden;
+    -ms-line-clamp: 2;
+    -webkit-line-clamp: 2;
+    line-clamp: 2;
+    display: -webkit-box;
+    display: box;
+    word-wrap: break-word;
+    -webkit-box-orient: vertical;
+    box-orient: vertical;
+  }
 
-.redactor-btn-template {
+  .redactor-btn-template {
     margin-bottom: 0px;
-}
+  }
 
-.download-btn-template {
+  .download-btn-template {
     background-color: transparent;
-}
+  }
 
-@media(max-width: 1200px) {
+  @media (max-width: 1200px) {
     max-width: 214px;
     max-height: 552px;
     .image-template {
-        max-width:212px;
+      max-width: 212px;
     }
-}
+  }
 
-@media(max-width: 1024px) {
-    /* margin: 0 auto; */
-}
-
-@media(max-width: 500px) {
-    /* margin: 0 auto; */
+  @media (max-width: 500px) {
     max-width: 168px;
     .card-template {
-        padding: 12px 12px 10px 12px;
-     }
-}
+      padding: 12px 12px 10px 12px;
+    }
+  }
 
-@media(max-width: 425px) {
+  @media (max-width: 425px) {
     max-width: 288px;
     max-height: 584px;
     .image-template {
-        max-width:286px;
+      max-width: 286px;
     }
-}
+  }
 `;
 
 export default StyledCard;
