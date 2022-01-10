43f0b261c0c33027b3683f4eabe492f5be46f307
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-01-10 18:07:21 +0300

    sub-cmp: fix link card items

--- components/card/sub-components/link.js
+++ components/card/sub-components/link.js
@@ -1,26 +1,30 @@
+import React from "react";
 import styled from "styled-components";
 import Link from "../../link";
 
 const ELink = styled(Link)`
-text-decoration: none;
-color: #444444;
-font-weight: 700;
-font-size: 16px;
-line-height: 22px;
+  text-decoration: none;
+  color: #444444;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
 
-&:hover {
-    color: #FF6F3D;
-}
+  &:hover {
+    color: #ff6f3d;
+  }
 
-@media(max-width: 768px) {
+  @media (max-width: 768px) {
     font-size: 14px;
-    line-height: 19px;  
-}
+    line-height: 19px;
+  }
 
-@media(max-width: 425px) {
+  @media (max-width: 425px) {
     font-size: 14px;
-    line-height: 19px; 
-}
+    line-height: 19px;
+  }
 `;
+const LinkCard = (props) => {
+  return <ELink target="_self" {...props} />;
+};
 
-export default ELink;
+export default LinkCard;
