84ff70fe49cc6e68aeaf5104d3f50c8191fa3668
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-01-26 16:21:23 +0300

    sub-cmp: fix footer

--- src/sub-components/footer-content/sub-components/mail-popup.js
+++ src/sub-components/footer-content/sub-components/mail-popup.js
@@ -1,161 +1,24 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import {
-  StyledMailForm,
-  CloseButton,
-  StyledMailPopup,
-} from "./styled-mail-popup";
-import Form from "../../../../components/form";
+import { CloseButton, StyledMailPopup } from "./styled-mail-popup";
 import Text from "../../../../components/text";
-import Portal from "../../../../components/portal";
 import Button from "../../../../components/button";
 import FForm from "../form";
 
-// TO DO: fix submit form
 const MailPopup = ({ t, language, active, setActive, submitForm, ...rest }) => {
   const [formComplete, setFormComplete] = useState(false);
-
-  const [nameValue, setNameValue] = useState("");
-  const [nameIsValid, setNameIsValid] = useState(true);
-  const [nameIsEmpty, setNameIsEmpty] = useState(true);
-  const [nameIsIncorrect, setNameIsIncorrect] = useState(true);
-  const [errorTextInput, setErrorTextInput] = useState(null);
-
-  const [emailValue, setEmailValue] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState(true);
-  const [emailIsEmpty, setEmailIsEmpty] = useState(true);
-  const [emailIsIncorrect, setEmailIsIncorrect] = useState(true);
-  const [errorMailInput, setErrorMailInput] = useState(null);
-  const [emailIsExist, setEmailIsExist] = useState(false);
-
-  const onKeyDownHandler = (e) => {
-    if (e.which === 13) {
-      onSubmitHandler(e);
-    }
-  };
-
-  const onTextChangeHandler = (e, isValid) => {
-    setNameValue(e.target.value);
-    setNameIsValid(isValid);
-  };
-
-  const onEmailChangeHandler = (e, isValid) => {
-    setEmailValue(e.target.value);
-    setEmailIsValid(isValid);
-    setEmailIsExist(false);
-  };
-
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    console.log("ok");
-    let hasError = false;
-
-    if (!nameValue.trim().length <= 3) {
-      hasError = true;
-      setNameIsEmpty(true);
-      setErrorTextInput(t("AuthErrorIndicationText"));
-      setNameIsValid(true);
-    } else {
-      setNameIsEmpty(false);
-      setNameIsValid(false);
-    }
-
-    if (!emailValue.trim()) {
-      hasError = true;
-      setEmailIsValid(false);
-      setEmailIsEmpty(true);
-      setErrorMailInput(t("AuthErrorIndicationText"));
-    } else {
-      setEmailIsEmpty(false);
-    }
-
-    if (emailValue.trim() && !emailIsValid) {
-      hasError = true;
-      setEmailIsValid(false);
-      setEmailIsIncorrect(true);
-      setErrorMailInput(t("AuthErrorIndicationIncorrectEmail"));
-    } else {
-      setEmailIsIncorrect(false);
-    }
-
-    if (hasError) return false;
-  };
-
-  const FormData = [
-    {
-      type: "heading",
-      headingText: "Don't Miss an Update!",
-      isHeader: true,
-    },
-    {
-      type: "other",
-      element: (
-        <Text className="captchaDescription">
-          Get the latest ONLYOFFICE news delivered to your inbox
-        </Text>
-      ),
-    },
-    {
-      type: "input",
-      inputType: "text",
-      placeholder: "First name",
-      callback: onTextChangeHandler,
-      value: nameValue,
-      isError: !nameIsEmpty,
-      errorText: errorTextInput,
-      height: "36px",
-      tabIndexProp: 0,
-    },
-    {
-      type: "other",
-      element: { errorTextInput } ? (
-        <Text height="12px" className="errorNameText">
-          {errorTextInput}
-        </Text>
-      ) : null,
-    },
-    {
-      type: "input",
-      inputType: "email",
-      placeholder: "Your email",
-      callback: onEmailChangeHandler,
-      isError:
-        (emailIsEmpty && !emailIsValid) ||
-        (emailIsIncorrect && !emailIsValid) ||
-        emailIsExist,
-      value: emailValue,
-      tabIndexProp: 0,
-      height: "36px",
-    },
-    {
-      type: "other",
-      element: { errorMailInput } ? (
-        <Text height="12px" className="errorMailText">
-          {errorMailInput}
-        </Text>
-      ) : null,
-    },
-    {
-      type: "button",
-      typeButton: "primary",
-      isSubmit: true,
-      toHideButton: false,
-      label: "Subscribe",
-      buttonClick: onSubmitHandler,
-    },
-  ];
 
   return (
     <StyledMailPopup active={active} onClick={() => setActive(false)} {...rest}>
       <div onClick={(e) => e.stopPropagation()} className="popup-content">
-      <div className="PopupPanelCaptionItems">
-        <div className="PopupPanelCaption">
+        <div className="PopupPanelCaptionItems">
+          <div className="PopupPanelCaption">
             <Text className="popupPanelText" label="Don't Miss an Update!" />
-          <CloseButton onClick={() => setActive(false)} />
+            <CloseButton onClick={() => setActive(false)} />
           </div>
           <div>
             {!formComplete ? (
-              <FForm setFormComplete={setFormComplete}/>
+              <FForm setFormComplete={setFormComplete} />
             ) : (
               <div className="success">
                 <div className="captchaDescription">
