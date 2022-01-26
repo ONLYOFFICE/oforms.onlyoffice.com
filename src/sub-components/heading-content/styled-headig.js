35df1c406659d528f455439531fd6ca7de4b0cb0
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-01-26 16:22:44 +0300

    sub-cmp: fix warning menu

--- src/sub-components/heading-content/styled-headig.js
+++ src/sub-components/heading-content/styled-headig.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { device } from "../../../components/utils/devices";
 
 const StyledHeadingContent = styled.div`
   background: ${(props) => (props.template ? `#F9F9F9` : `#333`)};
