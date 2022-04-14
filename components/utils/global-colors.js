dde469b1c992b306eafd01f8d41006d85fa28be4
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-04-14 14:33:48 +0300

    components added theme and utils
    * added base theme and theme provider
    * added utils: main-color and devices

---
+++ components/utils/global-colors.js
@@ -0,0 +1,17 @@
+const globalColors = {
+  white: "#ffffff",
+
+  gray: "#333",
+  grayMain: "#444444",
+  grayHover: "#555555",
+  grayTextInput: "#666666",
+  grayTextInputHover: "#F9F9F9",
+
+  grayMainLight: "#AAAAAA",
+  grayLight: "#CCCCCC",
+
+  orangeMain: "#ff6f3d",
+  orangeHover: "#ff865c",
+};
+
+export default globalColors;
