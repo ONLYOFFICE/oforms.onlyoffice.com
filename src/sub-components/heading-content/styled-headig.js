18d43b7729b18d260c64555cebb0343036bb81f8
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-04-14 13:53:08 +0300

    del gatsby

--- src/sub-components/heading-content/styled-headig.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const StyledHeadingContent = styled.div`
-  background: ${(props) => (props.template ? `#F9F9F9` : `#333`)};
-`;
-
-export default StyledHeadingContent;
