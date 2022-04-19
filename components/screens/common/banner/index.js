d921b14c0293434b46fac3c9f62b949840b988ef
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2022-04-19 14:13:02 +0300

    screens: fix banner cmp

--- components/screens/common/banner/index.js
+++ components/screens/common/banner/index.js
@@ -1,51 +1,19 @@
 import React from "react";
 
-import Box from "@components/common/box";
-import Link from "@components/common/link";
-import Text from "@components/common/text";
-import Button from "@components/common/button";
-import Heading from "@components/common/heading";
-import { Img } from "@components/common/image";
+import BannerOforms from "@components/common/banner";
+import StyledBanner from "./styled-banner";
 
-import StyledBanners from "./styled-banners";
-
-const BannerOforms = ({ t, currentLanguage }) => {
-  const linkReg = `https://www.onlyoffice.com/${currentLanguage}/registration.aspx`;
-  const linkDesktop = `https://www.onlyoffice.com/${currentLanguage}/download-desktop.aspx#desktop`;
-
+const Banner = ({ t, currentLanguage }) => {
   return (
-    <StyledBanners>
-      <Box
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="flex-start"
-        className="box-banner"
-      >
-        <Box className="box-banner-sec">
-          <Img src="/images/banners/oforms-banner.svg" width="36" height="48" />
-          <Heading level={3} className="banner_heading">
-            {t("BannerBuildYourOwnForms")}
-          </Heading>
-        </Box>
-        <Text isInline={false} className="text-banner">
-          {t("BannerUseToCreate")}
-        </Text>
-      </Box>
-      <Box className="banner_buttons" justifyContent="center">
-        <Link className="link" href={linkReg}>
-          <Button className="buttons" label={t("UseInTheCloud")} />
-        </Link>
-        <Link className="link" href={linkDesktop}>
-          <Button
-            className="buttons"
-            id="btn-type"
-            label={t("DownloadDesktopApp")}
-            typeButton="transparent"
-          />
-        </Link>
-      </Box>
-    </StyledBanners>
+    <StyledBanner
+      background="#333333"
+      padding="131px 0 111px"
+      tabletPadding="112px 0 109px"
+      mobileLPadding="48px 0 72px"
+    >
+      <BannerOforms t={t} currentLanguage={currentLanguage} />
+    </StyledBanner>
   );
 };
 
-export default BannerOforms;
+export default Banner;
