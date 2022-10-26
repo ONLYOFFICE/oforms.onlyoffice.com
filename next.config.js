f3a63fe6616edf1fd9b6646fce783ba81ee9e969
Vyacheslav-Belov-OO <vyacheslav.belov@onlyoffice.com>
2022-10-26 11:55:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate',
+          }
+        ], 
+      }, 
+    ]
+  },
   reactStrictMode: true,
   i18n,
   images: {
@@ -15,3 +29,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
