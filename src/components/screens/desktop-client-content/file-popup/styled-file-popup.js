6d6383ffe4f9ae40b411234ea8617f15df8fc0d4
khikmatazizov <khikmatjon.azizov@onlyoffice.com>
2023-06-03 16:48:04 +0500

    save

--- src/components/screens/desktop-client-content/file-popup/styled-file-popup.js
+++ src/components/screens/desktop-client-content/file-popup/styled-file-popup.js
@@ -6,7 +6,7 @@ const StyledFilePopup = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: ${({theme}) => theme.colors.palette.backgroundScrim};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   overflow-y: auto;
   z-index: 10;
