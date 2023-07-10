6ce247cceecabd809bf28b6d2b881dc2fa4f276b
Denis <denistyan16@gmail.com>
2023-07-10 15:12:10 +0500

    feat: add /form-submit page

---
+++ src/screens/form-submit-content/index.js
@@ -0,0 +1,280 @@
+import StyledFormSubmitContent from "./styled-form-submit-content";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Heading from "@common/heading";
+import Text from "@common/text";
+import Button from "@common/button";
+import Breadcrumb from "./sub-components/breadcrumb";
+import Select from "./sub-components/select";
+import Input from "./sub-components/input";
+import UploadFile from "./sub-components/upload-file";
+import UploadPopup from "./sub-components/upload-popup";
+import moment from "moment";
+import "moment/locale/fr";
+import "moment/locale/it";
+import "moment/locale/es";
+import "moment/locale/de";
+import "moment/locale/ja";
+import "moment/locale/zh-cn";
+
+const FormSubmitContent = ({ t, locale, categories }) => {
+  const categoriesData = categories.data.map((category) => category.attributes.categorie);
+  const languageData = [
+    { title: t("English"), key: "en" },
+    { title: t("Chinese (Simplified)"), key: "zh" },
+    { title: t("French"), key: "fr" },
+    { title: t("German"), key: "de" },
+    { title: t("Portuguese"), key: "pt" },
+    { title: t("Spanish"), key: "es" }
+  ];
+
+  const [file, setFile] = useState(undefined);
+  const [fileValue, setFileValue] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState([]);
+  const [language, setLanguage] = useState([]);
+  const [languageKey, setLanguageKey] = useState("");
+
+  const [nameValid, setNameValid] = useState(false);
+  const [descriptionValid, setDescriptionValid] = useState(false);
+  const [categoryValid, setCategoryValid] = useState(false);
+  const [languageValid, setLanguageValid] = useState(false);
+
+  const [nameFilled, setNameFilled] = useState(false);
+  const [descriptionFilled, setDescriptionFilled] = useState(false);
+  const [fileFilled, setFileFilled] = useState(false);
+
+  const [fileError, setFileError] = useState(true);
+  const [nameError, setNameError] = useState(true);
+  const [descriptionError, setDescriptionError] = useState(true);
+  const [categoryError, setCategoryError] = useState(true);
+  const [languageError, setLanguageError] = useState(true);
+
+  const [fileLoading, setFileLoading] = useState(false);
+  const [uploadPopup, setUploadPopup] = useState(false);
+  const [formValid, setFormValid] = useState(false);
+
+  const [selectedActive, setSelectedActive] = useState(new Array(categoriesData.length).fill(false));
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  // Form validation
+  useEffect(() => {
+    if (fileError || !(name.length > 0 && !nameError) || !(description.length > 0 && !descriptionError) || categoryError || languageError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    };
+  }, [fileError, name, description, nameError, descriptionError, categoryError, languageError]);
+
+  const onChangeHandler = (e) => {
+    switch (e.target.name) {
+      case "name":
+        setName(e.target.value);
+        !e.target.value.length < 1 && setNameError(false);
+        break;
+
+      case "description":
+        setDescription(e.target.value);
+        !e.target.value.length < 1 && setDescriptionError(false);
+        break;
+
+      case "file":
+        setFileValue(e.target.value);
+        !e.target.value.length < 1 && setFileError(false);
+        break;
+    };
+  };
+
+  const onFocusHandler = (e) => {
+    switch (e.target.name) {
+      case "name":
+        setNameError(false);
+        setNameValid(false);
+        break;
+
+      case "description":
+        setDescriptionError(false);
+        setDescriptionValid(false);
+        break;
+    };
+  };
+
+  const onBlurHandler = (e) => {
+    switch (e.target.name) {
+      case "name":
+        setNameFilled(true);
+
+        if (name && !nameError) {
+          setNameValid(true);
+        } else {
+          setNameValid(false);
+          setNameError(true);
+        };
+        break;
+
+      case "description":
+        setDescriptionFilled(true);
+
+        if (description && !descriptionError) {
+          setDescriptionValid(true);
+        } else {
+          setDescriptionValid(false);
+          setDescriptionError(true);
+        };
+        break;
+    };
+  };
+
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    axios.post(`https://oforms.teamlab.info/dashboard/api/oforms`, {
+      "data": {
+        "name_form": name,
+        "description_card": description,
+        "card_prewiew": "",
+        "template_image": "",
+        "file_oform": "",
+        "file_size": `${file.size.toString().substring(0, 2)} kB`,
+        "file_last_update": moment(file.lastModifiedDate).locale(languageKey).format(
+          languageKey === "zh" ? "Y年MM月DD" : languageKey === "ja" ? "Y年MM月DD日" : "MMMM D, y"
+        ),
+        "file_pages": "",
+        "template_desc": description,
+        "categorie": category,
+        "locale": languageKey,
+        "publishedAt": null
+      }
+    }).then((response) => {
+      console.log(response)
+    });
+
+    // Show upload popup
+    setUploadPopup(true);
+  };
+
+  const clearForm = () => {
+    setFile(undefined);
+    setFileValue("");
+    setName("");
+    setDescription("");
+    setCategory([]);
+    setLanguage([]);
+    setNameValid(false);
+    setDescriptionValid(false);
+    setCategoryValid(false);
+    setLanguageValid(false);
+    setFormValid(false);
+    setSelectedActive(new Array(categoriesData.length).fill(false));
+    setSelectedIndex(null);
+  };
+
+  return (
+    <StyledFormSubmitContent>
+      <Breadcrumb t={t} language={locale} />
+
+      <div className="wrapper">
+        <div className="upload-wrapper">
+          <UploadFile
+            t={t}
+            file={file}
+            setFile={setFile}
+            fileValue={fileValue}
+            setFileValue={setFileValue}
+            fileError={fileError}
+            setFileError={setFileError}
+            fileFilled={fileFilled}
+            setFileFilled={setFileFilled}
+            onChangeHandler={onChangeHandler}
+            fileLoading={fileLoading}
+            errorText={t("Upload file is empty")}
+          />
+        </div>
+        <div className="content">
+          <div className="wrapper-content">
+            <Heading className="title" level={3}>{t("Uploading your form")}</Heading>
+            <Text className="subtitle" as="p">{t("Please fill out all the fields before sending the form.")}</Text>
+            <Input
+              label={t("Form name")}
+              placeholder={t("Price quote template")}
+              errorText={(nameFilled && nameError) && t("Form name is empty")}
+              className={`${nameFilled && nameError ? "error" : ""} ${nameValid ? "valid" : ""}`}
+              name="name"
+              value={name}
+              onFocus={(e) => onFocusHandler(e)}
+              onChange={(e) => onChangeHandler(e)}
+              onBlur={(e) => onBlurHandler(e)}
+            />
+            <Input
+              isTextarea
+              label={t("Form description")}
+              placeholder={t("Give more details about your form, such as who will benefit from it, in which industry, etc.")}
+              errorText={(descriptionFilled && descriptionError) && t("Form description is empty")}
+              className={`${descriptionFilled && descriptionError ? "error" : ""} ${descriptionValid ? "valid" : ""}`}
+              name="description"
+              value={description}
+              onFocus={(e) => onFocusHandler(e)}
+              onChange={(e) => onChangeHandler(e)}
+              onBlur={(e) => onBlurHandler(e)}
+            />
+            <Select
+              isMulti
+              t={t}
+              label={t("Form category")}
+              labelMore={(t("(maximum 5)"))}
+              placeholder={t("Enter category or choose")}
+              errorText={t("Form category is empty")}
+              options={categoriesData}
+              selected={category}
+              setSelected={setCategory}
+              valid={categoryValid}
+              setValid={setCategoryValid}
+              error={categoryError}
+              setError={setCategoryError}
+              selectedActive={selectedActive}
+              setSelectedActive={setSelectedActive}
+            />
+            <Select
+              t={t}
+              label={t("Language")}
+              placeholder={t("Please select a language")}
+              errorText={t("Language is empty")}
+              options={languageData}
+              selected={language}
+              setSelected={setLanguage}
+              setLanguageKey={setLanguageKey}
+              valid={languageValid}
+              setValid={setLanguageValid}
+              error={languageError}
+              setError={setLanguageError}
+              selectedIndex={selectedIndex}
+              setSelectedIndex={setSelectedIndex}
+            />
+          </div>
+
+          <div className="file-info">
+            <div className="file-info-item">
+              <Text className="file-info-label file-type">{t("FileType")}:</Text>
+              <Text className="file-info-text">.docxf</Text>
+            </div>
+            <div className="file-info-item">
+              <Text className="file-info-label">{t("FileSize")}:</Text>
+              <Text className="file-info-text">{file !== undefined ? file.size.toString().substring(0, 2) : 0} kb</Text>
+            </div>
+            <div className="file-info-item">
+              <Text className="file-info-label">{t("Pages")}:</Text>
+              <Text className="file-info-text">0</Text>
+            </div>
+          </div>
+
+          <Button onClick={(e) => sendForm(e)} className="send-button" label={t("Send")} isDisabled={!formValid} />
+        </div>
+      </div>
+
+      <UploadPopup t={t} file={file} uploadPopup={uploadPopup} setUploadPopup={setUploadPopup} clearForm={clearForm} />
+    </StyledFormSubmitContent>
+  );
+};
+
+export default FormSubmitContent;
---
+++ src/screens/form-submit-content/sub-components/upload-popup/styled-upload-popup.js
@@ -0,0 +1,141 @@
+import styled from "styled-components";
+import { device } from "@components/utils/devices";
+import closeIcon from "@public/icons/close-btn.svg";
+import checkIcon from "@public/icons/check.svg";
+
+const StyledUploadPopup = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 16px;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+  overflow-y: auto;
+  opacity: 0;
+  visibility: hidden;
+  transition: opacity 0.3s, visibility 0.3s;
+
+  &.open {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  .popup-container {
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 100%;
+  }
+
+  .popup-wrapper {
+    padding: 40px;
+    width: 100%;
+    max-width: 600px;
+    box-shadow: 0px 20px 50px rgba(85, 85, 85, 0.15);
+    border-radius: 5px;
+    background-color: #FFFFFF;
+
+    @media ${device.mobile} {
+      padding: 16px;
+    }
+  }
+
+  .popup-top {
+    display: flex;
+    align-items: center;
+    margin-bottom: 32px;
+
+    @media ${device.mobile} {
+      margin-bottom: 24px;
+    }
+  }
+
+  .popup-title {
+    flex: 1 1 auto;
+    margin-right: 10px;
+    letter-spacing: -0.02em;
+  }
+
+  .popup-btn-close {
+    width: 24px;
+    height: 24px;
+    background-image: url(${closeIcon.src});
+    background-repeat: no-repeat;
+    background-size: 15px;
+    background-position: center;
+    cursor: pointer;
+  }
+
+  .upload-name {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 32px;
+    padding: 14px 22px;
+    min-height: 64px;
+    border: 1px dashed #CCCCCC;
+    border-radius: 5px;
+    background-color: #F9F9F9;
+  }
+
+  .upload-name-file {
+    padding-left: 40px;
+    font-size: 14px;
+    line-height: 32px;
+    background-image: url("https://static-oforms.onlyoffice.com/icons/oform.svg");
+    background-repeat: no-repeat;
+    background-size: 32px;
+    background-position: left center;
+
+    span {
+      font-weight: 700;
+    }
+  }
+
+  .upload-descr {
+    display: grid;
+    gap: 24px;
+    margin-bottom: 32px;
+
+    @media ${device.mobile} {
+      gap: 16px;
+    }
+  }
+
+  .upload-descr-item {
+    padding-left: 32px;
+    background-image: url(${checkIcon.src});
+    background-repeat: no-repeat;
+    background-size: 24px;
+    background-position: left top;
+  }
+
+  .upload-descr-title {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 26px;
+  }
+
+  .upload-text {
+    font-size: 16px;
+    line-height: 24px;
+
+    @media ${device.mobile} {
+      font-size: 14px;
+      line-height: 21px;
+    }
+  }
+
+  .popup-btn {
+    width: 100%;
+    font-size: 13px;
+    line-height: 17px;
+  }
+`;
+
+export default StyledUploadPopup;
