3e5dd5abfe4bfca1f1322ef9f96c2f18fb54f679
khikmatjon <khikmatjon@medio.uz>
2023-07-11 17:43:54 +0500

    header updated

---
+++ src/screens/common/header/header.jsx
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import {
+    HeaderBox,
+    HeaderForm,
+    HeaderInput,
+    HeaderInputWrapper,
+    HeaderStyled,
+    HeaderTitle
+} from "./header.styled";
+import CategorySelector from "@common/newCategorySelector";
+import LanguageSelector from "@common/languageSelector";
+import {SortSelector} from "@common/sortSelector";
+import {Loup, XClose} from "@icons";
+import {useRouter} from "next/router";
+import {useTranslation} from "next-i18next";
+
+const Header = (props) => {
+    const {
+        typeSortData,
+        locale,
+        category,
+        types,
+        categories,
+        compilations,
+        isDesktopClient,
+        categoryName,
+        queryDesktopClient,
+    } = props;
+    const [inputActive, setInputActive] = useState(!!queryDesktopClient)
+    const [value, setValue] = useState(queryDesktopClient ?? '')
+    const router = useRouter()
+    const theme = router.query.theme ?? 'theme-classic'
+    const {t} = useTranslation('common');
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(value.trim()) {
+            router.push({
+                pathname: '/searchresult',
+                query: {
+                    desktop: true,
+                    query: value.trim(),
+                    theme
+                }
+            })
+        } else onClear()
+    }
+
+    const onClear = () => {
+        setValue('')
+        router.push({
+            pathname: '/',
+            query: {
+                desktop: true,
+                theme
+            }
+        })
+    }
+
+    return (
+        <HeaderStyled>
+            <HeaderTitle>Templates</HeaderTitle>
+            <HeaderBox>
+                <CategorySelector
+                    typeSortData={typeSortData}
+                    className="form-control"
+                    types={types}
+                    categories={categories}
+                    compilations={compilations}
+                    isDesktopClient={isDesktopClient}
+                    categoryName={categoryName}
+                    queryDesktopClient={queryDesktopClient}
+                />
+                <HeaderInputWrapper>
+                    <HeaderForm onSubmit={onSubmit} active={inputActive}>
+                        <Loup size="24px" onClick={() => setInputActive(prevState => !prevState)} className="search-icon" />
+                        {
+                            <HeaderInput
+                                active={inputActive}
+                                value={value}
+                                onChange={(e) => setValue(e.target.value)}
+                                placeholder={t('Search')}
+                            />
+                        }
+                        {
+                            inputActive && value.trim().length !== 0 && <XClose onClick={onClear} size="30px" className="clear-icon" />
+                        }
+                    </HeaderForm>
+                    <SortSelector
+                        typeSortData={typeSortData}
+                        category={categoryName}
+                    />
+                    <LanguageSelector />
+                </HeaderInputWrapper>
+            </HeaderBox>
+        </HeaderStyled>
+    )
+}
+
+export default Header;
---
+++ src/screens/common/header/header.styled.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  padding-bottom: 21px;
+  padding-top: 41px;
+
+  .clear-icon {
+    color: ${({theme}) => theme.colors.palette.iconNormal};
+    position: absolute;
+    right: 0;
+  }
+
+  .search-icon {
+    color: ${({theme}) => theme.colors.palette.iconNormal};
+    margin-right: 4px;
+    cursor: pointer;
+  }
+`
+
+export const HeaderTitle = styled.h3`
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 32px;
+  margin: 0 0 13px 0;
+  color: ${({theme}) => theme.colors.palette.textNormal}
+`
+
+export const HeaderBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const HeaderInputWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+`
+
+export const HeaderForm = styled.form`
+  display: flex;
+  align-items: center;
+  height: 30px;
+  border-bottom: ${({active, theme}) => active ? `1px solid ${theme.colors.palette.borderToolbarButtonHover}` : undefined};
+  position: relative;
+`
+
+export const HeaderInput = styled.input`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  margin: 0;
+  width: ${({active}) => active ? '250px' : 0} ;
+  transition: width 300ms ease-in-out;
+  color: ${({theme}) => theme.colors.palette.textNormal};
+`
