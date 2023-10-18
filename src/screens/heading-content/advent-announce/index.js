42c99ec78992290ea447f8772dd27f57d616e318
DenisTyan <denistyan16@gmail.com>
2023-10-18 17:40:48 +0500

    fix(advent-announce): change to "ONLYOFFICE Docs v7.5 released"

--- src/screens/heading-content/advent-announce/index.js
+++ src/screens/heading-content/advent-announce/index.js
@@ -1,38 +1,30 @@
-import { useState, useEffect } from "react";
 import StyledAdventAnnounce from "./styled-advent-announce";
 import {useTranslation} from "next-i18next";
 
 const AdventAnnounce = ({currentLanguage}) => {
-  const [link, setLink] = useState('');
-  const hrefLang = `https://onlyoffice.com/blog/${currentLanguage === "zh" ? "zh-hans" : `${currentLanguage}`
-  }`;
   const { t } = useTranslation('common')
 
-  const setBlogLink = () => {
-    setLink(`2023/04/meet-onlyoffice-docspace/`)
-  };
-
-  useEffect(() => {
-    setBlogLink();
-  }, []);
- 
+  const href = 
+    currentLanguage === "fr" ? "https://www.onlyoffice.com/blog/fr/2023/10/sortie-onlyoffice-docs-7-5" :
+    currentLanguage === "de" ? "https://www.onlyoffice.com/blog/de/2023/10/onlyoffice-docs-7-5-veroeffentlicht" :
+    currentLanguage === "es" ? "https://www.onlyoffice.com/blog/es/2023/10/disponible-onlyoffice-docs-7-5" :
+    currentLanguage === "it" ? "https://www.onlyoffice.com/blog/it/2023/10/onlyoffice-docs-7-5" :
+    currentLanguage === "zh" ? "https://www.onlyoffice.com/blog/zh-hans/2023/10/onlyoffice-docs-7-5-released" : 
+    "https://www.onlyoffice.com/blog/2023/10/onlyoffice-docs-7-5-released";
 
   return (
-    <StyledAdventAnnounce  currentLanguage={currentLanguage}>
+    <StyledAdventAnnounce className={currentLanguage}>
       <div className="advent-announce advent-mobile-hide">        
-
-        <a href={`${hrefLang}/${link}`} target="_blank" rel="noreferrer noopener">
+        <a href={href} target="_blank" rel="noreferrer noopener">
           <div className="advent-announce-text">
-            <b>{t("ONLYOFFICE DocSpace released")}:</b>{t(" improve document collaboration with offices, customers, and partners.")}
-            <b>{t(" Use it for free!")}</b>
+            <b>{t("ONLYOFFICE Docs v7.5 released")}{`${currentLanguage === "zh" ? "：" : currentLanguage === "ja" ? "： " : ": "}`}</b>{t("PDF Editor, automatic hyphenation, Page Breaks and tracer arrows in sheets, Screen Readers, and more")}
           </div>
         </a>
       </div>
       <div className="advent-announce advent-desktop-hide">
-
-        <a href={`${hrefLang}${link}`} target="_blank" rel="noreferrer noopener">
+        <a href={href} target="_blank" rel="noreferrer noopener">
           <div className="advent-announce-text">
-            &nbsp;<b>{t("ONLYOFFICE DocSpace released")}</b>&nbsp;
+            <b>{t("ONLYOFFICE Docs v7.5 released")}</b>
           </div>
         </a>
       </div>
