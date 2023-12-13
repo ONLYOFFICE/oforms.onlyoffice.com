5b18a30f0f444d05d604808d847d683d6771e01b
khikmatjon <khikmatjon@medio.uz>
2023-12-13 14:33:11 +0500

    changed the skeleton appearance logic

--- src/screens/desktop-client-content/card/index.jsx
+++ src/screens/desktop-client-content/card/index.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import PropTypes from "prop-types";
 
-import {CardImage, CardStyled, CardTitle} from "./card.styled";
+import { CardFakeImgForLoading, CardImage, CardStyled, CardTitle } from './card.styled'
+import { Skeleton } from '../skeleton'
 
 const Card = (props) => {
     const {onClick, data, isDark} = props;
+    const [isLoaded, setIsLoaded] = useState(false)
 
 
     const img = data.attributes.card_prewiew?.data?.attributes?.url;
     const name = data.attributes.name_form;
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoaded(true)
+        }, 300)
+    }, [])
+
+    const onLoad = () => {
+        setIsLoaded(true)
+        console.log('onLoad image: ', name)
+    }
+
+    if(!isLoaded) {
+        return (
+            <>
+                <CardFakeImgForLoading
+                    src={img}
+                    onLoad={onLoad}
+                />
+                <Skeleton />
+            </>
+        )
+    }
+
+
+
     return (
         <CardStyled onClick={(e) => onClick(e, data)} isDark={isDark}>
             <CardImage
