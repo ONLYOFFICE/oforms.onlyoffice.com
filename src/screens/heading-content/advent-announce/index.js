91868b15fef8af14fbe080f11411527f4d62bc33
DenisTyan <denistyan16@gmail.com>
2024-01-31 10:59:28 +0500

    feat: update header menu, update advent-announce to Docs 8.0

--- src/screens/heading-content/advent-announce/index.js
+++ src/screens/heading-content/advent-announce/index.js
@@ -1,29 +1,32 @@
 import StyledAdventAnnounce from "./styled-advent-announce";
+import InternalLink from "@common/internal-link";
 
 const AdventAnnounce = ({ t, currentLanguage, stateMobile }) => {
   const href = 
-    currentLanguage === "fr" ? "https://www.onlyoffice.com/blog/fr/2023/12/onlyoffice-docspace-2-0" :
-    currentLanguage === "de" ? "https://www.onlyoffice.com/blog/de/2023/12/onlyoffice-docspace-2-0-veroeffentlicht" :
-    currentLanguage === "es" ? "https://www.onlyoffice.com/blog/es/2023/12/disponible-onlyoffice-docspace-2-0" :
-    currentLanguage === "pt" ? "https://www.onlyoffice.com/blog/pt-br/2023/12/onlyoffice-docspace-2-0-disponivel" :
-    currentLanguage === "it" ? "https://www.onlyoffice.com/blog/it/2023/12/onlyoffice-docspace-2-0" :
-    "https://www.onlyoffice.com/blog/2023/12/onlyoffice-docspace-2-0-released";
+    currentLanguage === "fr" ? "https://www.onlyoffice.com/blog/fr/2024/01/onlyoffice-docs-8-0" :
+    currentLanguage === "de" ? "https://www.onlyoffice.com/blog/de/2024/01/onlyoffice-docs-8-0-veroeffentlicht" :
+    currentLanguage === "es" ? "https://www.onlyoffice.com/blog/es/2024/01/onlyoffice-docs-8-0" :
+    currentLanguage === "pt" ? "https://www.onlyoffice.com/blog/pt-br/2024/01/onlyoffice-docs-8-0" :
+    currentLanguage === "it" ? "https://www.onlyoffice.com/blog/it/2024/01/onlyoffice-docs-8-0" :
+    currentLanguage === "ja" ? "https://www.onlyoffice.com/blog/ja/2024/01/onlyoffice-docs-8-0-released" :
+    currentLanguage === "zh" ? "https://www.onlyoffice.com/blog/zh-hans/2024/01/onlyoffice-docs-8-0-released" :
+    "https://www.onlyoffice.com/blog/2024/01/onlyoffice-docs-8-0-released";
 
   return (
     <StyledAdventAnnounce className={`${currentLanguage} ${stateMobile ? "active": ""}`}>
       <div className="advent-announce advent-mobile-hide">
-        <a href={href} target="_blank" rel="noreferrer noopener">
+        <InternalLink href={href}>
           <div className="advent-announce-text">
-            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceDesktop")}} />
+            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceDesktop")}}></span>
           </div>
-        </a>
+        </InternalLink>
       </div>
       <div className={`advent-announce advent-desktop-hide ${stateMobile ? "is-open": ""}`}>
-        <a href={href} target="_blank" rel="noreferrer noopener">
+        <InternalLink href={href}>
           <div className="advent-announce-text">
-            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceMobile")}} />
+            <span dangerouslySetInnerHTML={{ __html: t("AdventAnnounceMobile")}}></span>
           </div>
-        </a>
+        </InternalLink>
       </div>
     </StyledAdventAnnounce>
   )
