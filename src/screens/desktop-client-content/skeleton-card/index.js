bdbc137f1039b3916369b2888cf139f2c25c8b74
DenisTyan <denistyan16@gmail.com>
2024-03-20 17:27:19 +0500

    refactor(desktop-client-content): refactor "skeleton" component

---
+++ src/screens/desktop-client-content/skeleton-card/index.js
@@ -0,0 +1,12 @@
+import StyledSkeletonCard from "./styled-skeleton-card";
+
+const SkeletonCard = ({ theme }) => {
+  return (
+    <StyledSkeletonCard theme={theme}>
+      <div className="skeleton-card-body"></div>
+      <div className="skeleton-card-title"></div>
+    </StyledSkeletonCard>
+  );
+};
+
+export default SkeletonCard;
