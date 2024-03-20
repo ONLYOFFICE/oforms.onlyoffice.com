b879d61582c6b43f205d596b9b5ea66068779ad8
DenisTyan <denistyan16@gmail.com>
2024-03-20 17:03:17 +0500

    refactor(_app): remove ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,6 @@
-import {appWithTranslation} from "next-i18next";
-import "../src/style/global-style.css";
-import {useMemo} from "react";
-import darkTheme from "../src/style/themes/darkTheme.json";
-import contrastDarkTheme from "../src/style/themes/contrastDarkTheme.json";
-import classicTheme from "../src/style/themes/classicTheme.json";
-import lightTheme from '../src/style/themes/lightTheme.json';
-import {useRouter} from "next/router";
-import {ThemeProvider} from "styled-components";
-import {Base} from "@components/themes";
+import { appWithTranslation } from "next-i18next";
+import "@src/style/global-style.css";
 
-const App = ({Component, pageProps}) => {
-    const router = useRouter();
-    const mode = router.query.theme;
-    const isDesktopClient = router.query.desktop
-    const theme = useMemo(() => {
-        if(isDesktopClient && mode !== undefined) {
-            switch (mode) {
-                case 'theme-light': return lightTheme;
-                case 'theme-dark': return darkTheme;
-                case 'theme-contrast-dark': return contrastDarkTheme;
-            }
-        }
-
-        return classicTheme;
-    }, [mode, isDesktopClient])
-    return (
-        <ThemeProvider theme={{...Base, ...theme}}>
-            <Component {...pageProps} />
-        </ThemeProvider>
-    )
-}
+const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default appWithTranslation(App);
