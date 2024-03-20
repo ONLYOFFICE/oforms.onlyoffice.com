958d5d1863eb1c23bb1fffadc187cd545ab8ecd1
DenisTyan <denistyan16@gmail.com>
2024-03-20 17:37:13 +0500

    refactor(searchresult-page): refactor searchresult page

--- pages/searchresult.js
+++ pages/searchresult.js
@@ -1,143 +1,110 @@
-import { useState, useEffect, lazy, Suspense } from "react";
-import { useRouter } from "next/router";
-import axios from "axios";
+import { useState } from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import getAllForms from "@lib/strapi/getForms";
 import getAllTypes from "@lib/strapi/getTypes";
 import getAllCategories from "@lib/strapi/getCategories";
 import getAllCompilations from "@lib/strapi/getCompilations";
-
 import CONFIG from "@config/config";
 import Layout from "@components/layout";
-import HeadSEO from "../src/screens/head-content";
-import HeadingContent from "../src/screens/heading-content";
-import InfoContent from "../src/screens/search-result-page/info-content";
-import MainContent from "../src/screens/search-result-page/main-content";
-import DesktopClientContent from "../src/screens/desktop-client-content";
-import AdventAnnounce from "../src/screens/heading-content/advent-announce";
+import HeadSEO from "@src/screens/head-content";
+import HeadingContent from "@src/screens/heading-content";
+import InfoContent from "@src/screens/search-result-page/info-content";
+import MainContent from "@src/screens/search-result-page/main-content";
+import DesktopClientContent from "@src/screens/desktop-client-content";
+import AdventAnnounce from "@src/screens/heading-content/advent-announce";
+import Accordion from "@src/screens/common/accordion";
+import Footer from "@src/screens/footer-content";
 
-const Accordion = lazy(() => import("../src/screens/common/accordion"), {
-  loading: () => <div />,
-});
-const Footer = lazy(() => import("../src/screens/footer-content"), {
-  loading: () => <div />,
-});
-
-const SearchResult = ({
-  locale,
-  sort,
-  page,
-  types, 
-  categories, 
-  compilations,
-}) => {
+const SearchResult = ({ locale, sort, page, types, categories, compilations, isDesktopClient, searchQuery, searchData, theme }) => {
   const { t } = useTranslation("common");
-
-  const [searchResults, setSearchResults] = useState([]);
-  const router = useRouter();
-  const query = router.query.query
-  const queryDesktopClient = router.query.query
-
-  const isDesktop = router.query.desktop?.substr(0, 4);
-  const [isDesktopClient, setIsDesktopClient] = useState(isDesktop);
   const [stateMobile, setStateMobile] = useState(false);
 
-  const CMSConfigAPI = CONFIG.api.cms || "http://localhost:1337";
-  const searchReqData = () => {
-    const searchQuery = locale === "en" || locale === "fr" || locale === "pt" ? query.toLowerCase() === "curriculum vitae" || query.toLowerCase() === "curriculum" || query.toLowerCase() === "vitae" ? "cv" : query : query;
-    const searchURL = `${CMSConfigAPI}/api/oforms/?sort=name_form:${sort}&populate[0]=categories&locale=${locale === "pt" ? "pt-br" : locale}&filters[name_form][$containsi]=${searchQuery}&populate=template_image&populate=file_oform&populate=categories&populate=card_prewiew`;
-    axios
-      .get(searchURL)
-      .then((response) => {
-        setSearchResults(response.data);
-      })
-      .catch((error) => console.log(error));
-  };
-
-  useEffect(() => {
-    if (query?.length >= 2) {
-      searchReqData();
-    } else {
-      setSearchResults(null);
-    }
-  }, [router]);
-
-
-  return isDesktopClient ?
-    <Layout>
-      <Layout.PageHead>
-        <HeadSEO
-          title={t("titleIndexPage")}
-          metaSiteNameOg={t("metaSiteNameOg")}
-          metaDescription={t("titleIndexPage")}
-          metaDescriptionOg={t("metaDescriptionOgIndexPage")}
-          metaKeywords={t("metaKeywordsIndexPage")}
-          isDesktopClient={isDesktopClient}
-        />
-      </Layout.PageHead>
-      <DesktopClientContent
-        currentLanguage={locale}
-        data={searchResults}
-        sort={sort}
-        page={+page}
-        types={types}
-        categories={categories}
-        compilations={compilations}
-        isDesktopClient={isDesktopClient}
-        queryDesktopClient={queryDesktopClient}
-      />
-    </Layout>
-  :
-    <Layout>
-      <Layout.PageHead>
-        <HeadSEO
-          title={t("titleIndexPage")}
-          metaSiteNameOg={t("metaSiteNameOg")}
-          metaDescription={t("titleIndexPage")}
-          metaDescriptionOg={t("metaDescriptionOgIndexPage")}
-          metaKeywords={t("metaKeywordsIndexPage")}
-        />
-      </Layout.PageHead>
-      <Layout.PageAnnounce>
-        <AdventAnnounce t={t} currentLanguage={locale} stateMobile={stateMobile} />
-      </Layout.PageAnnounce>
-      <Layout.PageHeader>
-        <HeadingContent t={t} currentLanguage={locale} stateMobile={stateMobile} setStateMobile={setStateMobile} />
-      </Layout.PageHeader>
-      <Layout.SectionMain>
-        <InfoContent query={query} />
-        <MainContent
-          currentLanguage={locale}
-          sort={sort}
-          data={searchResults}
-          page={+page}
-          types={types}
-          categories={categories}
-          compilations={compilations}
-        />
-        <Suspense>
+  return (
+    isDesktopClient ? (
+      <Layout>
+        <Layout.PageHead>
+          <HeadSEO
+            title={t("titleIndexPage")}
+            metaSiteNameOg={t("metaSiteNameOg")}
+            metaDescription={t("titleIndexPage")}
+            metaDescriptionOg={t("metaDescriptionOgIndexPage")}
+            metaKeywords={t("metaKeywordsIndexPage")}
+            isDesktopClient={isDesktopClient}
+          />
+        </Layout.PageHead>
+        <Layout.SectionMain>
+          <DesktopClientContent
+            t={t}
+            currentLanguage={locale}
+            data={searchData}
+            sort={sort}
+            types={types}
+            categories={categories}
+            compilations={compilations}
+            isDesktopClient={isDesktopClient}
+            queryDesktopClient={searchQuery}
+            theme={theme}
+          />
+        </Layout.SectionMain>
+      </Layout>
+    ) : (
+      <Layout>
+        <Layout.PageHead>
+          <HeadSEO
+            title={t("titleIndexPage")}
+            metaSiteNameOg={t("metaSiteNameOg")}
+            metaDescription={t("titleIndexPage")}
+            metaDescriptionOg={t("metaDescriptionOgIndexPage")}
+            metaKeywords={t("metaKeywordsIndexPage")}
+          />
+        </Layout.PageHead>
+        <Layout.PageAnnounce>
+          <AdventAnnounce t={t} currentLanguage={locale} stateMobile={stateMobile} />
+        </Layout.PageAnnounce>
+        <Layout.PageHeader>
+          <HeadingContent t={t} currentLanguage={locale} stateMobile={stateMobile} setStateMobile={setStateMobile} />
+        </Layout.PageHeader>
+        <Layout.SectionMain>
+          <InfoContent t={t} query={searchQuery} />
+          <MainContent
+            t={t}
+            currentLanguage={locale}
+            sort={sort}
+            data={searchData}
+            page={+page}
+            types={types}
+            categories={categories}
+            compilations={compilations}
+          />
           <Accordion currentLanguage={locale} />
-        </Suspense>
-      </Layout.SectionMain>
-      <Layout.PageFooter>
-        <Suspense>
-          <Footer t={t} locale={locale}/>
-        </Suspense>
-      </Layout.PageFooter>
-    </Layout>
+        </Layout.SectionMain>
+        <Layout.PageFooter>
+          <Footer t={t} locale={locale} />
+        </Layout.PageFooter>
+      </Layout>
+    )
+  )
 };
 
 export const getServerSideProps = async ({ locale, query }) => {
-  const isDesktop = query.desktop === "true";
+  const isDesktopClient = query.desktop === "true";
+  const theme = query.theme;
   const page = query.page || 1;
-  const sort = query._sort || 'asc'
-  const pageSize = query.pageSize || isDesktop ? 0 : 9;
+  const sort = query._sort || "asc";
+  const pageSize = query.pageSize || isDesktopClient ? 0 : 9;
   const forms = await getAllForms(locale === "pt" ? "pt-br" : locale, page, sort, pageSize);
   const types = await getAllTypes(locale === "pt" ? "pt-br" : locale);
   const categories = await getAllCategories(locale === "pt" ? "pt-br" : locale);
   const compilations = await getAllCompilations(locale === "pt" ? "pt-br" : locale);
-  
+  const searchQuery = query.query || "";
+
+  const searchName = locale === "en" || locale === "fr" || locale === "pt" ? searchQuery?.toLowerCase() === "curriculum vitae" || searchQuery?.toLowerCase() === "curriculum" || searchQuery?.toLowerCase() === "vitae" ? "cv" : searchQuery : searchQuery;
+  const searchRes = await fetch(
+    `${CONFIG.api.cms}/api/oforms/?sort=name_form:${sort}&populate[0]=categories&locale=${locale === "pt" ? "pt-br" : locale}&filters[name_form][$containsi]=${searchName}&populate=template_image&populate=file_oform&populate=categories&populate=card_prewiew`
+  );
+  const searchData = await searchRes.json();
+
   return {
     props: {
       ...(await serverSideTranslations(locale, "common")),
@@ -147,7 +114,11 @@ export const getServerSideProps = async ({ locale, query }) => {
       sort,
       types,
       categories,
-      compilations
+      compilations,
+      isDesktopClient,
+      searchQuery,
+      searchData: searchQuery === "" ? [] : searchData,
+      theme: isDesktopClient ? theme || "" : null
     },
   };
 };
