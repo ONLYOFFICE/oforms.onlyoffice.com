119648e881eef209b7c5304f582b81ee399f2a75
samirkerimov <84935773+Sam-oops@users.noreply.github.com>
2025-04-02 20:30:25 +0500

    Updated arabian translation and select "forms" -> "templates"

--- components/screens/desktop-client/category-selector/index.js
+++ components/screens/desktop-client/category-selector/index.js
@@ -172,7 +172,7 @@ const CategorySelector = ({ t, locale, categories, types, compilations, isCatego
                 level={5} 
                 className={`category-selector-item ${isTypeOpen ? "active" : ""}`}
               >
-                {t("Forms by type")}
+                {t("Templates by type")}
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" clipRule="evenodd" d="M13.5436 11.9975L9.29363 16.2574C8.90355 16.6483 8.90192 17.2838 9.28998 17.6768L9.31705 17.7042C9.70511 18.0972 10.3359 18.0988 10.726 17.7078L15.7058 12.7165C15.9258 12.496 16.0223 12.1977 15.9949 11.909C15.9768 11.6784 15.8799 11.4531 15.7044 11.2772L10.731 6.2923C10.3409 5.90132 9.71014 5.90294 9.32208 6.29591L9.29502 6.32332C8.90696 6.71629 8.90859 7.3518 9.29866 7.74277L13.5436 11.9975Z" fill="#444444"/>
                 </svg>
@@ -192,7 +192,7 @@ const CategorySelector = ({ t, locale, categories, types, compilations, isCatego
                         <path fillRule="evenodd" clipRule="evenodd" d="M10.4564 11.9975L14.7064 16.2574C15.0964 16.6483 15.0981 17.2838 14.71 17.6768L14.683 17.7042C14.2949 18.0972 13.6641 18.0988 13.274 17.7078L8.29417 12.7165C8.07416 12.496 7.97772 12.1977 8.00508 11.909C8.02321 11.6784 8.12008 11.4531 8.29557 11.2772L13.269 6.2923C13.6591 5.90132 14.2899 5.90294 14.6779 6.29591L14.705 6.32332C15.093 6.71629 15.0914 7.3518 14.7013 7.74277L10.4564 11.9975Z" fill="#444444"/>
                       </svg>
                     </button>
-                    <Heading className="category-selector-title" level={4} label={t("Forms by type")} />
+                    <Heading className="category-selector-title" level={4} label={t("Templates by type")} />
                     <button onClick={handleClose} className="category-selector-header-btn">
                       <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M5.78497 5.2945C5.39445 5.68502 5.39077 6.32186 5.78129 6.71239L11.0689 12L11.069 12.0001L5.78138 17.2877C5.39086 17.6782 5.39454 18.3151 5.78506 18.7056C6.17559 19.0961 6.81243 19.0998 7.20295 18.7093L12.4906 13.4217L17.7782 18.7093C18.1687 19.0998 18.8055 19.0961 19.1961 18.7056C19.5866 18.3151 19.5903 17.6782 19.1997 17.2877L13.9121 12.0001L13.9122 12L19.1998 6.71239C19.5903 6.32186 19.5867 5.68502 19.1961 5.2945C18.8056 4.90397 18.1688 4.90029 17.7782 5.29082L12.4906 10.5785L12.4906 10.5785L12.4905 10.5785L7.20287 5.29082C6.81234 4.90029 6.1755 4.90397 5.78497 5.2945Z" fill="#808080"/>
