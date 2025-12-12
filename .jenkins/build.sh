fbeca8e0d3b0d5e6452bb32a7d6e20571eff6874
DenisTyan <denistyan16@gmail.com>
2025-12-12 18:25:35 +0500

    feat: update dependencies, build.sh and api requests, remove yarn.lock

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -68,7 +68,7 @@ cd "$BUILD_DIR"
 
 # Build application with error handling
 echo "Starting application build..."
-docker run --rm -e NODE_OPTIONS="--max-old-space-size=4096" -v "$BUILD_DIR":"$APP_DIR" -w "$APP_DIR" "$DOCKER_CONTAINER_TAG" sh -c "yarn && yarn build"
+docker run --rm -e NODE_OPTIONS="--max-old-space-size=4096" -v "$BUILD_DIR":"$APP_DIR" -w "$APP_DIR" "$DOCKER_CONTAINER_TAG" sh -c "npm i && npm run build"
 if [ $? -ne 0 ]; then
     echo "Error: Application build failed. Cleaning up build directory and exiting."
     send_telegram_notification "FAILED"
@@ -109,11 +109,11 @@ if docker ps -a | grep -wq "$APP_NAME"; then
     else
         # Build and run a new container if the image tag does not match
         docker rm "$APP_NAME"
-        docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" yarn start
+        docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" npm run start
     fi
 else
         # Build and run a new container if it doesn't exist
-        docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" yarn start
+        docker run -d --name "$APP_NAME" --publish "0.0.0.0:$EXPOSE_PORT:$EXPOSE_PORT" -v "$APP_DIR:$APP_DIR" -w "$APP_DIR" --restart always "$DOCKER_CONTAINER_TAG" npm run start
 fi
 
 # Send success notification
