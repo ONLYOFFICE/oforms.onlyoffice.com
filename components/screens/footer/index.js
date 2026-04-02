91374cb05e227d051b8d2603958514e2e8a05b6d
DenisTyan <denistyan16@gmail.com>
2026-04-02 13:16:29 +0500

    fix(footer): fix language links

--- components/screens/footer/index.js
+++ components/screens/footer/index.js
@@ -12,7 +12,9 @@ const Footer = ({ locale }) => {
       languages={languages.map((language) => ({
         shortKey: language.shortKey,
         name: language.longKey,
-        href: router.asPath
+        href: router.pathname === "/form-submit" || router.pathname === "/searchresult" ||
+            router.pathname === "/pdf-form-templates" || router.pathname === "/document-templates" ||
+            router.pathname === "/spreadsheet-templates" || router.pathname === "/presentation-templates" ? router.asPath : "/"
       }))}
       base={{
         url: "https://www.onlyoffice.com",
