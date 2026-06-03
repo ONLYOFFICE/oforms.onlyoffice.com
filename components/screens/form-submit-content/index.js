cbe8e639f0ffa9161f96b69f96f83cf54796d23f
DenisTyan <denistyan16@gmail.com>
2026-06-03 14:00:43 +0500

    fix: fix upload api

--- components/screens/form-submit-content/index.js
+++ components/screens/form-submit-content/index.js
@@ -198,7 +198,12 @@ const FormSubmitContent = ({ t, locale, categories, formExts, queryIndexData }) 
     formData.append("description", description);
     formData.append("languageKey", languageKey);
     formData.append("categoryId", categoryId);
-    formData.append("formExt", [formExts.data.find(d => d.attributes.ext === fileName?.match(/\.(\w+)$/)?.[1]).id]);
+    const formExtId = formExts?.data?.find(
+      (d) => d.attributes.ext === fileName?.match(/\.(\w+)$/)?.[1],
+    )?.id;
+    if (formExtId) {
+      formData.append("formExt", [formExtId]);
+    }
     formData.append("fileOrientation", fileOrientation);
 
     const sendFormResponse = await axios.post(queryIndexData?.[5] ? "/api/form-upload-submission" : "/api/form-submission", formData);
